5658bcfb230a49195e1db4c68efba245dc78482f
dimacia666 <dimahotab666@gmail.com>
2023-08-22 18:08:23 +0300

    final version

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,18 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import templateFunction from '../templates/item-markup.hbs';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryRefs = document.querySelector('.gallery');
+
+(function createMarkup() {
+  const itemMarkup = galleryItems.map(templateFunction).join('');
+  galleryRefs.insertAdjacentHTML('beforeend', itemMarkup);
+})();
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionData: 'alt', 
+  captionDelay: 250,
+});
+
